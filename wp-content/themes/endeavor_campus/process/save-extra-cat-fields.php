3a0c52305e84038f58a297df859ba599e4e46906
Juan Malagon <nicolas0915@hotmail.com>
2021-01-07 10:47:08 -0500

    Functionality for update extra category fields in BD

---
+++ wp-content/themes/endeavor_campus/process/save-extra-cat-fields.php
@@ -0,0 +1,18 @@
+<?php 
+
+function edited_category_extra_fields( $term_id ){
+    if( isset( $_POST['cat_meta'] ) ){
+        $term_id = $term_id;
+        $cat_meta = $cat_meta = get_term_meta($term_id);
+
+        $cat_keys = array_keys( $_POST['cat_meta'] );
+
+        foreach( $cat_keys as $key ){
+            if( isset( $_POST['cat_meta'][$key] ) ){
+                $cat_meta[$key] = $_POST['cat_meta'][$key];
+
+                update_term_meta( $term_id, $key, $_POST['cat_meta'][$key] );
+            }
+        }        
+    }
+}
