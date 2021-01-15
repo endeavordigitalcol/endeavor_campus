b6933fff670ac4ee78412ab56a0d2bade3b9bd2a
Juan Malagon <nicolas0915@hotmail.com>
2021-01-14 21:39:42 -0500

    Support for related content added

--- wp-content/themes/endeavor_campus/single.php
+++ wp-content/themes/endeavor_campus/single.php
@@ -82,78 +82,77 @@ if (have_posts()) {
             </div>
         </div>
         <div class="row pt-4">
-            <div class="col-md-4 p-3">
-                <div class="lc-card">
-                    <div class="lc-card--header">
-                        <img class="w-100" src="https://via.placeholder.com/300x169" alt="">
-                        <hr class="lc-card--overlay finance">
-                        </hr>
-                    </div>
-                    <div class="lc-card--body">
-                        <h5 class="lc-card--cat-name color-teal">
-                            Finanzas
-                        </h5>
-                        <img src="<?php echo get_theme_file_uri() ?>/assets/images/lc-card.png" alt="">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ex quam, fermentum eget orci sed, tempor finibus turpis.
-                        </p>
-                    </div>
-                    <div class="lc-card--footer">
-                        <p class="lc-card--date">10/01/2020</p>
-                        <div class="lc-card--time">
-                            <p class="color-gray">8 min</p>
+            <?php
+
+            $post_term = get_the_terms($post->ID, 'custom_post_sector');
+
+            $cr_args = array(
+                'posts_per_page'    => 3,
+                'tax_query'         => array(
+                    array(
+                        'taxonomy'  => 'custom_post_sector',
+                        'field'     => 'name',
+                        'terms'     => $post_term[0]->name
+                    )
+                ),
+                'post__not_in'      => array($post->ID)
+            );
+
+            $cr_query = new WP_Query($cr_args);
+
+            if ($cr_query->have_posts()) {
+                while ($cr_query->have_posts()) {
+                    $cr_query->the_post();
+                    $cat = get_the_category(); ?>
+
+
+                    <div class="col-md-4 p-3">
+                        <div class="lc-card">
+                            <div class="lc-card--header">
+                                <?php
+                                if (has_post_thumbnail()) {
+                                    the_post_thumbnail('thumbnail', ['class' => 'w-100']);
+                                }
+                                ?>
+                                <hr class="lc-card--overlay <?php echo $cat[0]->slug ?>" />
+                            </div>
+                            <div class="lc-card--body">
+                                <h5 class="lc-card--cat-name color-teal">
+                                    <a href="<?php echo home_url('/') . 'categorias/' . $cat[0]->slug ?>">
+                                        <?php
+                                        if ($cat[0]->name == 'Cultura y RRHH') {
+                                            echo 'Cultura y recursos humanos';
+                                        } else {
+                                            echo $cat[0]->name;
+                                        }
+                                        ?>
+                                    </a>
+                                </h5>
+                                <img src="<?php echo get_theme_file_uri() ?>/assets/images/lc-card.png" alt="">
+                                <p> <?php the_title() ?></p>
+                            </div>
+                            <div class="lc-card--footer">
+                                <p class="lc-card--date"><?php echo date('d/m/Y', strtotime($post->post_date)) ?></p>
+                                <div class="lc-card--time">
+                                    <p class="color-gray">
+                                        <?php
+                                        if (get_post_meta($post->ID, 'Tiempo de lectura', true) != '') {
+                                            echo get_post_meta($post->ID, 'Tiempo de lectura', true) . ' min';
+                                        }
+                                        ?>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-md-4 p-3">
-                <div class="lc-card">
-                    <div class="lc-card--header">
-                        <img class="w-100" src="https://via.placeholder.com/300x169" alt="">
-                        <hr class="lc-card--overlay economy">
-                        </hr>
-                    </div>
-                    <div class="lc-card--body">
-                        <h5 class="lc-card--cat-name color-teal">
-                            Economía
-                        </h5>
-                        <img src="<?php echo get_theme_file_uri() ?>/assets/images/lc-card.png" alt="">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ex quam, fermentum eget orci sed, tempor finibus turpis.
-                        </p>
-                    </div>
-                    <div class="lc-card--footer">
-                        <p class="lc-card--date">10/01/2020</p>
-                        <div class="lc-card--time">
-                            <p class="color-gray">8 min</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 p-3">
-                <div class="lc-card">
-                    <div class="lc-card--header">
-                        <img class="w-100" src="https://via.placeholder.com/300x169" alt="">
-                        <hr class="lc-card--overlay digital">
-                        </hr>
-                    </div>
-                    <div class="lc-card--body">
-                        <h5 class="lc-card--cat-name color-teal">
-                            Digital
-                        </h5>
-                        <img src="<?php echo get_theme_file_uri() ?>/assets/images/lc-card.png" alt="">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ex quam, fermentum eget orci sed, tempor finibus turpis.
-                        </p>
-                    </div>
-                    <div class="lc-card--footer">
-                        <p class="lc-card--date">10/01/2020</p>
-                        <div class="lc-card--time">
-                            <p class="color-gray">8 min</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
+            <?php
+                }
+            }
+
+            wp_reset_postdata();
+
+            ?>
         </div>
     </div>
 </section>
