04cfdbcfa4bdf08c80e07fa300539148730d09d8
Juan Malagon <nicolas0915@hotmail.com>
2021-01-26 15:04:00 -0500

    Support for sharing contents

--- wp-content/plugins/rate-posts/assets/js/share-post.js
+++ wp-content/plugins/rate-posts/assets/js/share-post.js
@@ -5,5 +5,20 @@
             method: 'share',
             href: $(location).attr('href'),
         }, function (response) { });
-    })
+    });
+    $('#tw-share').click(() => {
+        let tw_url = 'https://twitter.com/share?url=';
+        window.open( tw_url + $(location).attr('href') );
+    });
+    $('#in-share').click(() => {
+        let in_url = 'https://www.linkedin.com/sharing/share-offsite/?url=';
+        window.open( in_url + $(location).attr('href') );
+    });
+    $('#copy-link').click(() => {
+        let $temp = $("<input>");
+        $('#copy-link').append($temp);
+        $temp.val($(location).attr('href')).select();
+        document.execCommand("copy");
+        $temp.remove();
+    });
 })(jQuery);
--- wp-content/plugins/rate-posts/process/rate-post-template.php
+++ wp-content/plugins/rate-posts/process/rate-post-template.php
@@ -1,27 +1,39 @@
-<div class="rating-post mt-4">
-    <h3>RATE_I18N</h3>
-    <ul class="list-unstyled rating-list" READONLY_PLACEHOLDER id="rating-list" data-rateit-value="POST_RATING" data-pid="POST_ID">
-        <li>
-            <span class="icon-star"></span>
-        </li>
-        <li>
-            <span class="icon-star"></span>
-        </li>
-        <li>
-            <span class="icon-star"></span>
-        </li>
-        <li>
-            <span class="icon-star"></span>
-        </li>
-        <li>
-            <span class="icon-star"></span>
-        </li>
-    </ul>
-</div>
-<div class="share-post">
-    <ul class="list-unstyled share-post-links">
-        <li id="fb-share">
-            <span class="icon-Facebook"></span>
-        </li>
-    </ul>
+<div class="row pt-4 mt-4 border-top-gray">
+    <div class="col-md-5 rating-post">
+        <h6 class="mt-1">RATE_I18N</h6>
+        <ul class="list-unstyled rating-list" READONLY_PLACEHOLDER id="rating-list" data-rateit-value="POST_RATING" data-pid="POST_ID">
+            <li>
+                <span class="icon-star"></span>
+            </li>
+            <li>
+                <span class="icon-star"></span>
+            </li>
+            <li>
+                <span class="icon-star"></span>
+            </li>
+            <li>
+                <span class="icon-star"></span>
+            </li>
+            <li>
+                <span class="icon-star"></span>
+            </li>
+        </ul>
+    </div>
+    <div class="col-md-5 share-post">
+        <h6 class="mt-1">¡Compártelo!</h6>
+        <ul class="list-unstyled share-post-links">
+            <li id="fb-share">
+                <span class="icon-Facebook"></span>
+            </li>
+            <li id="tw-share">
+                <span class="icon-Twitter"></span>
+            </li>
+            <li id="in-share">
+                <span class="icon-Linkedin"></span>
+            </li>
+            <li id="copy-link">
+                <span class="icon-Copy"></span>
+            </li>
+        </ul>
+    </div>
 </div>
