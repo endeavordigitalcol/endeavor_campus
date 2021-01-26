d7f26f71268b197047cbdb1b8b0934c2dc093cc2
Juan Malagon <nicolas0915@hotmail.com>
2021-01-26 11:40:14 -0500

    Support for sharing content in FB

--- wp-content/themes/endeavor_campus/inc/partials/seo-meta-sn.php
+++ wp-content/themes/endeavor_campus/inc/partials/seo-meta-sn.php
@@ -5,14 +5,14 @@
 
 <!-- Open Graph / Facebook -->
 <meta property="og:type" content="website">
-<meta property="og:url" content="<?php echo home_url() ?>">
+<meta property="og:url" content="<?php echo get_permalink($post->ID) ?>">
 <meta property="og:title" content="<?php echo $post->post_title ?>">
 <meta property="og:description" content="<?php echo $post->post_excerpt ?>">
 <meta property="og:image" content="<?php the_post_thumbnail_url($post->ID) ?>">
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="<?php echo home_url() ?>">
+<meta property="twitter:url" content="<?php echo get_permalink($post->ID) ?>">
 <meta property="twitter:title" content="<?php echo $post->post_title ?>">
 <meta property="twitter:description" content="<?php echo $post->post_excerpt ?>">
 <meta property="twitter:image" content="<?php the_post_thumbnail_url($post->ID) ?>">
