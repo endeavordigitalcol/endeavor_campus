f452d45b4f3424c4bd7a54e52e61128bb8c083d5
Juan Malagon <nicolas0915@hotmail.com>
2021-01-25 19:27:03 -0500

    Style updated

--- wp-content/plugins/rate-posts/process/rate-post-template.php
+++ wp-content/plugins/rate-posts/process/rate-post-template.php
@@ -1,4 +1,4 @@
-<h3 class="mt-5">RATE_I18N</h3>
+<h3 class="mt-4 pt-3 rating-post">RATE_I18N</h3>
 <ul class="list-unstyled rating-list" READONLY_PLACEHOLDER id="rating-list" data-rateit-value="POST_RATING" data-pid="POST_ID">
     <li>
         <span class="icon-star"></span>
