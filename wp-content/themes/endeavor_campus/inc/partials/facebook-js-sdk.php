ddf524604ae96909ab167eea602847e660560316
Juan Malagon <nicolas0915@hotmail.com>
2021-01-26 12:10:17 -0500

    Support for sharing content in FB

---
+++ wp-content/themes/endeavor_campus/inc/partials/facebook-js-sdk.php
@@ -0,0 +1,11 @@
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: '835966360285788',
+            autoLogAppEvents: true,
+            xfbml: true,
+            version: 'v9.0'
+        });
+    };
+</script>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
