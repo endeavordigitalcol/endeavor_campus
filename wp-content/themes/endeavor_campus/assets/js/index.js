40b84caf3758f721e34bc840beb371d33be3d7b9
Juan Malagon <nicolas0915@hotmail.com>
2021-01-28 16:26:17 -0500

    Error with swiper fixed

--- wp-content/themes/endeavor_campus/assets/js/index.js
+++ wp-content/themes/endeavor_campus/assets/js/index.js
@@ -1,26 +1,28 @@
 (function($){
-    const swiperCatHome = new Swiper('.swiper-cat-home', {
-        slidesPerView: 1, 
-        spaceBetween: 16,
-        loop: true,
-        speed: 1500,
-        navigation: {
-            nextEl: '.icon-slider-arrow-right',
-            prevEl: '.icon-slider-arrow-left'
-        },
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false
-        },
-        breakpoints: {
-            600: {
-                slidesPerView: 2
+    if( $('.swiper-cat-home').length ){
+        const swiperCatHome = new Swiper('.swiper-cat-home', {
+            slidesPerView: 1, 
+            spaceBetween: 16,
+            loop: true,
+            speed: 1500,
+            navigation: {
+                nextEl: '.icon-slider-arrow-right',
+                prevEl: '.icon-slider-arrow-left'
             },
-            1024: {
-                slidesPerView: 3
+            autoplay: {
+                delay: 4000,
+                disableOnInteraction: false
+            },
+            breakpoints: {
+                600: {
+                    slidesPerView: 2
+                },
+                1024: {
+                    slidesPerView: 3
+                }
             }
-        }
-    })
+        })
+    }
 
     $( window ).load(() => {
         same_height('.lc-card--body', 32);
